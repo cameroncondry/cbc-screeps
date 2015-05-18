2886cc22e6bb1dfb63060e13aa857801686beec2
Cameron Condry <ccondry@pltw.org>
2015-05-18 12:02:41 -0400

    .

--- game/harvest.js
+++ game/harvest.js
@@ -2,12 +2,7 @@ module.exports = function (creep) {
     var sources = creep.room.find(FIND_SOURCES);
     var spawn = Game.spawns.Spawn1;
 
-    if (creep.energy == creep.energyCapacity) {
-        creep.moveTo(spawn);
-        creep.transferEnergy(spawn);
 
-        return;
-    }
 
     var carrier = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
         filter: function (creep) {
@@ -15,8 +10,15 @@ module.exports = function (creep) {
         }
     });
 
-    if (carrier) {
-        creep.transferEnergy(carrier);
+    if (carrier[0]) {
+        creep.transferEnergy(carrier[0]);
+    }
+
+    if (creep.energy == creep.energyCapacity) {
+        creep.moveTo(spawn);
+        creep.transferEnergy(spawn);
+
+        return;
     }
 
     creep.moveTo(sources[3]);
