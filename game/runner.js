516c255995ce2e028e8657b4cb5bc6898bcd94f2
Cameron Condry <ccondry@pltw.org>
2015-05-18 13:33:05 -0400

    .

--- game/runner.js
+++ game/runner.js
@@ -1,12 +1,13 @@
 module.exports = function (creep) {
     var spawn = Game.spawns.Spawn1;
     var sources = creep.room.find(FIND_DROPPED_ENERGY);
-    var enemies = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 4);
+    var enemies = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 6);
 
     if (creep.energy < creep.energyCapacity && !enemies.length) {
         if (!creep.pos.isNearTo(sources[0])) creep.moveTo(sources[0]);
 
         creep.pickup(sources[0]);
+        return;
     }
 
     if (creep.energy == creep.energyCapacity || (!sources[0] && creep.energy > 0)) {
@@ -16,7 +17,5 @@ module.exports = function (creep) {
         return;
     }
 
-    if (!sources[0]) {
-        creep.moveTo(spawn.pos.x + 2, spawn.pos.y);
-    }
+    creep.moveTo(spawn.pos.x + 2, spawn.pos.y);
 };
