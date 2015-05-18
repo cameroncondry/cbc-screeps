ba53ab84b0cd95dcac5f6fb6ed6c2492010cee5e
Cameron Condry <ccondry@pltw.org>
2015-05-17 22:12:20 -0400

    Debugging spawning errors.

--- game/clock.js
+++ game/clock.js
@@ -19,6 +19,8 @@ module.exports = function () {
     }
 
     var spawnCreep = function (modules, type) {
+        console.log(spawn.createCreep(modules, undefined, {module: type}));
+
         if (spawn.createCreep(modules, undefined, {module: type})) {
             console.log('created ' + type);
         }
