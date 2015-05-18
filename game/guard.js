440e672bcb098a6de345cdd8ba8a68eb182e1e95
Cameron Condry <ccondry@pltw.org>
2015-05-18 12:34:59 -0400

    .

--- game/guard.js
+++ game/guard.js
@@ -2,8 +2,13 @@ module.exports = function (creep) {
     var spawn = Game.spawns.Spawn1;
     var targets = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
 
-    if (targets.length && creep.hits / creep.hitsMax > 0.4) {
+    if (targets.length) {
         creep.rangedAttack(targets[0]);
+
+        if (creep.hits / creep.hitsMax < 0.4) {
+            creep.moveTo(creep.pos.x, creep.pos.y - 3);
+            return;
+        }
 
         var enemy = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 2);
 
