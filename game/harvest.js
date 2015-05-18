5de82f097bc7e71fa2018be8a022ce7431fa84ea
Cameron Condry <ccondry@pltw.org>
2015-05-18 11:54:23 -0400

    Removed carry from harvesters until needed.

--- game/harvest.js
+++ game/harvest.js
@@ -1,5 +1,13 @@
 module.exports = function (creep) {
     var sources = creep.room.find(FIND_SOURCES);
+    var spawn = Game.spawns.Spawn1;
+
+    if (creep.energy == creep.energyCapacity) {
+        creep.moveTo(spawn);
+        creep.transferEnergy(spawn);
+
+        return;
+    }
 
     creep.moveTo(sources[3]);
     creep.harvest(sources[3]);
