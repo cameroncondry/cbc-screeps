37ec0164b3036def9581e870d99a5253cae9e1f2
Cameron Condry <ccondry@pltw.org>
2015-05-18 12:01:43 -0400

    .

--- game/harvest.js
+++ game/harvest.js
@@ -9,6 +9,16 @@ module.exports = function (creep) {
         return;
     }
 
+    var carrier = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
+        filter: function (creep) {
+            return creep.memory.module == 'runner';
+        }
+    });
+
+    if (carrier) {
+        creep.transferEnergy(carrier);
+    }
+
     creep.moveTo(sources[3]);
     creep.harvest(sources[3]);
 };
