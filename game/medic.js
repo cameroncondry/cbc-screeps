388ffd8f0f9a3a8a1aa88c3fb8238232860b976f
Cameron Condry <ccondry@pltw.org>
2015-05-17 22:54:12 -0400

    Send medics back towards guards.

--- game/medic.js
+++ game/medic.js
@@ -2,15 +2,23 @@ module.exports = function (creep) {
     var spawn = Game.spawns.Spawn1;
 
     var target = creep.pos.findClosest(FIND_MY_CREEPS, {
-        filter: function (object) {
-            console.log(object.memory.module);
-
-            return object.hits < object.hitsMax;
+        filter: function (creep) {
+            return creep.hits < creep.hitsMax;
         }
     });
 
     if (target) {
         creep.moveTo(target);
         creep.heal(target);
+    }
+
+    target = creep.pos.findClosest(FIND_MY_CREEPS, {
+       filter: function (creep) {
+           return creep.memory.module == 'guard';
+       }
+    });
+
+    if (target) {
+        creep.moveTo(target);
     }
 };
